a1281b651322436c86f5c502c880516a44c00a47
caleb-wo <camawolfe52@icloud.com>
2025-04-30 14:10:25 -0600

    Working on routing assignment

--- server.js
+++ server.js
@@ -1,6 +1,39 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Variable Declaration
+
+/**
+ * Declare important (semi-global) variables
+ */
+const PORT = process.env.PORT || 3000;
+
+// Create __dirname and __filename variables
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// -------------------- //
+
+
  
 const app = express();
+/**
+ * Routes
+ */
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/src/views/home.html'));
+});
+ 
+app.get('/page1', (req, res) => {
+    res.sendFile(path.join(__dirname, '/src/views/page1.html'));
+});
+ 
+app.get('/page2', (req, res) => {
+    res.sendFile(path.join(__dirname, '/src/views/page2.html'));
+});
  
 const name = process.env.NAME; // <-- NEW
  
@@ -8,7 +41,6 @@ app.get('/', (req, res) => {
     res.send(`Hello, ${name}!`); // <-- UPDATED
 });
  
-const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://127.0.0.1:${PORT}`);
 });
